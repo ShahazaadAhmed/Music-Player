a209c027be91163a959782f3542d6ae1025f1749
ShahazaadAhmed <110083695+ShahazaadAhmed@users.noreply.github.com>
2024-09-18 12:42:50 +0530

    Update mp.py

--- mp.py
+++ mp.py
@@ -1,3 +1,4 @@
+import os
 import tkinter
 import tkinter.font
 import customtkinter as ck
@@ -6,6 +7,7 @@ import math
 import time
 from threading import *
 from PIL import Image, ImageTk
+from tkinter import filedialog
 root=ck.CTk()
 root.title('Music Player')
 root.geometry('350x470')
@@ -13,6 +15,7 @@ pygame.mixer.init()
 n=0
 ip = False
 sk=None
+t1 = None
 los=['songs/Unravel Piano(Version).mp3','songs/Sudden Shower.mp3','songs/Unlasting.mp3','songs/Call of Silence.mp3']
 lot=['img/unravel.png','img/ssimg.jpg','img/unlasting.jpeg','img/Call of Silence.jpeg']
 
@@ -25,19 +28,28 @@ def gt(q,w):
     th=tkinter.Label(im, image=load)
     th.image=load
     th.place(relx=0.02,rely=0.02)
-    sn=q[6:-4]
+    sn = os.path.basename(q[:-4]) 
     if sk is None:
         sk=tkinter.Label(text=sn,bg='#2b2b2b',fg='white',font=('Lucida Sans Unicode',13,'bold'))
         sk.place(relx=0.5,rely=0.62,anchor='center')
     else:
         sk.config(text=sn)    
-
+       
+def upload_song():
+    song_path = filedialog.askopenfilename(title="Select a Song", filetypes=(("MP3 files", "*.mp3"),))
+    if song_path:
+        los.append(song_path)
+        print(f"Added song: {song_path}")
+    image_path = filedialog.askopenfilename(title="Select an Image", filetypes=(("Image files", "*.png;*.jpg;*.jpeg"),))
+    if image_path:
+        lot.append(image_path)
+        print(f"Added image: {image_path}")
 def prs():
     a=pygame.mixer.Sound(f'{los[n]}')
     sl=a.get_length()*3
     for i in range(0,math.ceil(sl)):
         time.sleep(0.3)
-        pro.set(pygame.mixer.music.get_pos()/1000000)
+        pro.set(pygame.mixer.music.get_pos()/1000)
 
 def threading():
     t1=Thread(target=prs)
@@ -84,7 +96,13 @@ def back():
 
 def vl(value):
     pygame.mixer.music.set_volume(value)
-def stps():
+def lop():
+    pass
+def on_closing():
+    global t1
+    if t1 is not None:
+        t1.join()
+    pygame.mixer.music.stop()
     pygame.mixer.quit()
     root.destroy()
 
@@ -113,9 +131,10 @@ pl=ck.CTkButton(root,text='▶',width=10,height=10,fg_color='#242424',hover_colo
 pl.place(relx=0.1,rely=0.91)
 vol=ck.CTkSlider(root,width=210,height=18,from_=0 ,to=1,progress_color='#a61641',button_color='white',button_hover_color='white',command=vl)
 vol.pack(pady=10)
-br=ck.CTkButton(root,text='X',width=10,height=10,fg_color='#242424',hover_color='#333333',command=stps)
-br.place(relx=0.85,rely=0.91)
-
-root.protocol("WM_DELETE_WINDOW", lambda: [pygame.mixer.music.stop(), pygame.mixer.quit(), root.destroy()])
+br=ck.CTkButton(root,text='X',width=10,height=10,fg_color='#242424',hover_color='#333333',command=lop)
+br.place(relx=0.83,rely=0.91)
+us=ck.CTkButton(root,text='♪',width=5,height=5,fg_color='#242424',hover_color='#333333',font=ck.CTkFont(weight='bold'),command=upload_song)
+us.place(relx=0.77,rely=0.054)
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 root.mainloop()
